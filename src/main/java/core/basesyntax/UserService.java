dbc8797940e80fc9711348f3691defcc77b78814
Vitalik Kopach <apple_air@ukr.net>
2020-12-03 15:06:47 +0200

    added class UserNotFoundException and changed class UserService

--- src/main/java/core/basesyntax/UserService.java
+++ src/main/java/core/basesyntax/UserService.java
@@ -1,8 +1,39 @@
 package core.basesyntax;
 
+import core.basesyntax.exception.UserNotFoundException;
+import java.util.Arrays;
+
 public class UserService {
 
     public int getUserScore(String[] records, String email) {
-        return 0;
+        int userScore = 0;
+
+        if (isNotExist(records,email) != true) {
+            userScore = getScore(records,email);
+        }
+
+        return userScore;
+    }
+
+    private static boolean isNotExist(String[] data, String email) {
+        boolean emailIsFound = Arrays.asList(data).contains(email);
+
+        if (data.length == 0 | emailIsFound) {
+            throw new UserNotFoundException("User with given email doesn't exist");
+        }
+
+        return emailIsFound;
+    }
+
+    private static int getScore(String[] data, String email) {
+        int score = 0;
+        for (int i = 0; i < data.length; i++) {
+            int index = data[i].indexOf(':');
+            if (data[i].substring(0, index).equals(email)) {
+                score = Integer.parseInt(data[i].replaceAll("[^0-9]", ""));
+                return score;
+            }
+        }
+        return score;
     }
 }
