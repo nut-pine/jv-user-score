4a2e65fa4f62e168251395fe352de6147b163741
Vitalik Kopach <apple_air@ukr.net>
2020-12-04 07:59:30 +0200

    some changes in method getUserScore

--- src/main/java/core/basesyntax/UserService.java
+++ src/main/java/core/basesyntax/UserService.java
@@ -7,18 +7,14 @@ public class UserService {
     public int getUserScore(String[] records, String email) {
         int score = 0;
         String[] splitRecords;
-        String[] splitEmail = email.split(":");
 
         for (int i = 0; i < records.length; i++) {
             splitRecords = records[i].split(":");
-            if (splitRecords[0].equals(splitEmail[0])) {
-                score = Integer.parseInt(splitRecords[1]);
+            if (splitRecords[0].equals(email)) {
+                return score = Integer.parseInt(splitRecords[1]);
             }
         }
-
-        //if splitEmail.length > 1, than we get wrong email format
-
-        if (score == 0 | splitEmail.length > 1) {
+        if (score == 0) {
             throw new UserNotFoundException("User with given email doesn't exist");
         }
         return score;
