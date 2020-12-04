204d0c5d988c8c957dffd525678a166b56641158
Vitalik Kopach <apple_air@ukr.net>
2020-12-04 12:45:57 +0200

    deleted 'if checked'

--- src/main/java/core/basesyntax/UserService.java
+++ src/main/java/core/basesyntax/UserService.java
@@ -6,18 +6,13 @@ public class UserService {
 
     public int getUserScore(String[] records, String email) {
         String[] splitRecords;
-        boolean isNotContain = true;
 
-        for (int i = 0; i < records.length; i++) {
-            splitRecords = records[i].split(":");
-            if (splitRecords[0].equals(email)) {
-                isNotContain = false;
+        for (String s : records) {
+            splitRecords = s.split(":");
+            if (splitRecords.length == 2 && splitRecords[0].equals(email)) {
                 return Integer.parseInt(splitRecords[1]);
             }
         }
-        if (isNotContain) {
-            throw new UserNotFoundException("User with given email doesn't exist");
-        }
-        return 0;
+        throw new UserNotFoundException("User with given email doesn't exist");
     }
 }
