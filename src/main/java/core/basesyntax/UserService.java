1e80ebd62613ee43ff2288ed326fe9915524552c
Vitalik Kopach <apple_air@ukr.net>
2020-12-04 12:38:43 +0200

    changed method getUserScore()

--- src/main/java/core/basesyntax/UserService.java
+++ src/main/java/core/basesyntax/UserService.java
@@ -6,14 +6,16 @@ public class UserService {
 
     public int getUserScore(String[] records, String email) {
         String[] splitRecords;
+        boolean isNotContain = true;
 
         for (int i = 0; i < records.length; i++) {
             splitRecords = records[i].split(":");
             if (splitRecords[0].equals(email)) {
+                isNotContain = false;
                 return Integer.parseInt(splitRecords[1]);
             }
         }
-        if (records.length == 0 || email.contains(":")) {
+        if (isNotContain) {
             throw new UserNotFoundException("User with given email doesn't exist");
         }
         return 0;
